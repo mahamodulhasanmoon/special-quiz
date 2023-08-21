61e80827495769617057c5fc083fb265a3e36dda
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-08-21 15:13:30 +0600

    d

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const app = require('./app');
+const specialQuizRouter = require('../modules/specialQuizes/specialQuiz.routes');
 const routes = express.Router();
 
 
+app.use(specialQuizRouter)
 // check health routes
-routes.get("/", (_req, res) => {
+routes.get("/health", (_req, res) => {
     res.status(200).json({ 
       status:200,
        message: "success" 
---
+++ modules/specialQuizes/specialQuiz.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const specialQuizRouter = express.Router();
+
+
+
+module.exports = specialQuizRouter
